4e251aa311a2571aa93abb0571309926119fe5d4
Dipak Yadav <Dipak.Yadav@neudesic.com>
2021-01-06 14:40:46 +0530

    updated terraform

--- Terraform/webapp-db.tf
+++ Terraform/webapp-db.tf
@@ -37,7 +37,7 @@ resource "azurerm_app_service" "app-service" {
 }
 
 resource "azurerm_sql_server" "sqldb" {
-  name                         = "terraform-sqlserver"
+  name                         = "terraform-sqlserver1"
   resource_group_name          = "${azurerm_resource_group.rg.name}"
   location                     = "${azurerm_resource_group.rg.location}"
   version                      = "12.0"
@@ -54,4 +54,41 @@ resource "azurerm_sql_database" "db" {
   tags = {
     environment = "production"
   }
+}
+
+resource "azurerm_virtual_network" "example" {
+  name                = "terraform-network"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+resource "azurerm_subnet" "example" {
+  name                 = "frontend"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.example.name
+  address_prefixes     = ["10.0.2.0/24"]
+}
+
+resource "azurerm_network_security_group" "example" {
+  name                = "example-nsg"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  security_rule {
+    name                       = "test123"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "*"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
+
+resource "azurerm_subnet_network_security_group_association" "example" {
+  subnet_id                 = azurerm_subnet.example.id
+  network_security_group_id = azurerm_network_security_group.example.id
 }
